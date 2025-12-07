5956039e1647e1417bea3a836e2c2ab466768cb4
Marco Wessels <marcowessels01@gmail.com>
2025-12-08 10:24:29 +1300

    Initial Commit

---
+++ Swift Regression/Swift_RegressionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_RegressionApp.swift
+//  Swift Regression
+//
+//  Created by Marco Wessels on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Swift_RegressionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
